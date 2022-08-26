3f6c416a362ac597fa8cedfc95f160813833078d
Tam Dang <tam.dang@power.trade>
2022-08-26 07:58:00 +0000

    commit all first

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
-#include <boost/iostreams/device/mapped_file.hpp>
-#include <boost/iostreams/stream.hpp>
-#include <vector>
-using namespace boost::iostreams;
+
 int main() {
-    mapped_file_params params;
-    params.path = "test.txt";
-    params.flags = mapped_file::readwrite;
-    params.new_file_size = 1ul << 10;
-    boost::iostreams::stream<mapped_file_sink> writter{params};
-    std::vector<std::string> v(2000);
-    v[0] = "Tam is here";
-    std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>(writter, "\n"));
     return 0;
 }
+
